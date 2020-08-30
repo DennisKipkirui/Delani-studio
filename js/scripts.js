bdbb62cc029f82e7c6078dd7070ce2d67ae834d5
Dennis <denismutai95@gmail.com>
2020-08-30 15:50:13 +0300

    create toggle functions

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".design1").click(function() {
+      $(".design1").toggle();
+      $(".details1").toggle();
+    });
+    $(".details1").click(function() {
+      $(".details1").toggle();
+      $(".design1").toggle();
+    });
+    $(".design2").click(function() {
+      $(".design2").toggle();
+      $(".details2").toggle();
+    });
+    $(".details2").click(function() {
+      $(".details2").toggle();
+      $(".design2").toggle();
+    });
